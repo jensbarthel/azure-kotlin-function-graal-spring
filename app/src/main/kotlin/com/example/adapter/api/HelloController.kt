b77f6cd9491af23a4174f8e3d5b3c3fb24b75235
Jens Barthel <j.barthel@neusta.de>
2023-11-10 10:07:39 +0100

    Simplify controller

--- app/src/main/kotlin/com/example/adapter/api/HelloController.kt
+++ app/src/main/kotlin/com/example/adapter/api/HelloController.kt
@@ -2,15 +2,13 @@ package com.example.adapter.api
 
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HelloController {
-    @GetMapping("hello/{id}")
+    @GetMapping("hello")
     fun handleGetHello(
-        @PathVariable id: String
     ): ResponseEntity<String> {
-        return ResponseEntity("Hello $id", null, 200)
+        return ResponseEntity("Hello friend", null, 200)
     }
 }
